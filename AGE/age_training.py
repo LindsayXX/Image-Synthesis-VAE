9abb4f0214aba96d08317e76305a26373c762286
AlexLacson <alexander_nou@hotmail.com>
2019-05-06 18:17:32 +0200

    Age network ready for reproduction (I think)

--- AGE/age_training.py
+++ AGE/age_training.py
@@ -5,6 +5,7 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 import torch.utils.data as data
 import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
 from AGE.age_networks import *
 from AGE.loss_functions import *
 from AGE.tools import *
@@ -12,17 +13,21 @@ import sys
 import os
 
 root_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+if not os.path.exists('age_model_cifar'):
+    os.mkdir('age_model_cifar')
+if not os.path.exists('age_plot_cifar'):
+    os.mkdir('age_plot_cifar')
 model_dir = os.path.join(root_dir, 'age_model_cifar')
 plot_dir = os.path.join(root_dir, 'age_plot_cifar')
 
 if __name__ == '__main__':
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    #trainset.data = trainset.data[np.where(np.array(trainset.targets)==1)] # Only cars
+    trainset.data = trainset.data[np.where(np.array(trainset.targets)==1)] # Only cars
     #indice = list(range(0, 10000))
     # sampler=data.SubsetRandomSampler(indice)
     #trainset = torchvision.datasets.SVHN(root='.\data', transform=transform, download =True)
-    trainloader = data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=2)
+    trainloader = data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=0, pin_memory=False, drop_last=True)
     #testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     #testset_sub = torch.utils.data.SubsetRandomSampler(indice)
     #testloader = data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=2)
@@ -35,15 +40,22 @@ if __name__ == '__main__':
     DROP_LR = 40
     LR = 0.0002
 
-    cudnn.benchmark = True
-
     age_E = age_enc(z_dim=Z_DIM).cuda()
     age_G = age_gen(z_dim=Z_DIM).cuda()
+    age_E.apply(weights_init)
+    age_G.apply(weights_init)
+    age_E.train()
+    age_G.train()
 
-    KL_min = KL_Loss_AGE(minimize=True).cuda()
-    KL_max = KL_Loss_AGE(minimize=False).cuda()
-    loss_l1 = nn.L1Loss().cuda()
-    loss_l2 = nn.MSELoss().cuda()
+    x = torch.FloatTensor(64, 3, 32, 32).cuda()
+    z_sample = torch.FloatTensor(64, Z_DIM, 1, 1).cuda()
+    x = Variable(x)
+    z_sample = Variable(z_sample)
+
+    KL_min = KL_Loss_AGE(minimize=True)
+    KL_max = KL_Loss_AGE(minimize=False)
+    loss_l1 = nn.L1Loss()
+    loss_l2 = nn.MSELoss()
 
     age_optim_E = optim.Adam(age_E.parameters(), lr=LR, betas=(0.5, 0.999))
     age_optim_G = optim.Adam(age_G.parameters(), lr=LR, betas=(0.5, 0.999))
@@ -54,6 +66,8 @@ if __name__ == '__main__':
     gen_fake_z = []
     gen_rec_z = []
 
+    cudnn.benchmark = True
+
     for epoch in tqdm(range(NUM_EPOCH)):
         if epoch % DROP_LR == (DROP_LR - 1):
             LR /= 2
@@ -62,13 +76,24 @@ if __name__ == '__main__':
 
             for param_group in age_optim_G.param_groups:
                 param_group['lr'] = LR
-            torch.save(age_E.state_dict(), f"{model_dir}/encoder_{epoch}")
-            torch.save(age_G.state_dict(), f"{model_dir}/generator_{epoch}")
+
+            state_E = {
+                'epoch': epoch,
+                'state_dict': age_E.state_dict(),
+                'optimizer': age_optim_E.state_dict(),
+            }
+            state_G = {
+                'epoch': epoch,
+                'state_dict': age_G.state_dict(),
+                'optimizer': age_optim_G.state_dict(),
+            }
+            torch.save(state_E, f"{model_dir}/encoder_{epoch}")
+            torch.save(state_G, f"{model_dir}/generator_{epoch}")
         for i, data in enumerate(trainloader, 0):
             #print(f'Epoch: {epoch+1}, Batch: i+1')
             #print('--------------------------------')
-            x, label = data
-            x = x.cuda()
+            input, label = data
+            x.data.copy_(input)
             batch_size = list(x.size())[0]
             # Update encoder
             loss_E = []
@@ -84,7 +109,7 @@ if __name__ == '__main__':
             loss_E.append(x_rec_loss)
             enc_rec_x.append(x_rec_loss)
 
-            z_sample = sampling(batch_size, Z_DIM).cuda()
+            z_sample.copy_(sampling(batch_size, Z_DIM))
             x_fake = age_G(z_sample).detach()
             z_fake = age_E(x_fake)
             KL_z_fake = KL_max(z_fake)
@@ -101,7 +126,7 @@ if __name__ == '__main__':
                 loss_G = []
                 age_optim_G.zero_grad()
 
-                z_sample = sampling(batch_size, Z_DIM).cuda()
+                z_sample.copy_(sampling(batch_size, Z_DIM))
                 x_fake = age_G(z_sample)
                 z_fake = age_E(x_fake)
                 KL_z_fake = KL_min(z_fake)
@@ -115,38 +140,40 @@ if __name__ == '__main__':
                 sum(loss_G).backward()
                 age_optim_G.step()
 
-                #print(f'Generator: KL fake z - {KL_z_fake}, Rec z loss - {z_rec_loss}')
+                # print(f'Generator: KL fake z - {KL_z_fake}, Rec z loss - {z_rec_loss}')
 
-    # plt.plot(enc_z, label='Encoder KL z')
-    # plt.plot(enc_fake_z, label='Encoder KL fake z')
-    # plt.legend()
-    # plt.savefig()
-    # #plt.show()
-    # plt.plot(gen_fake_z, label='Generator KL fake z')
-    # plt.legend()
-    # plt.show()
-    # plt.plot(enc_rec_x, label='Encoder x recon')
-    # plt.legend()
-    # plt.show()
-    # plt.plot(gen_rec_z, label='Generator z recon')
-    # plt.legend()
-    # plt.show()
+    state_E = {
+        'epoch': epoch,
+        'state_dict': age_E.state_dict(),
+        'optimizer': age_optim_E.state_dict(),
+    }
+    state_G = {
+        'epoch': epoch,
+        'state_dict': age_G.state_dict(),
+        'optimizer': age_optim_G.state_dict(),
+    }
+    torch.save(state_E, f"{model_dir}/encoder_{epoch}")
+    torch.save(state_G, f"{model_dir}/generator_{epoch}")
 
-    # with torch.no_grad():
-    #     for i in range(5):
-    #         z_sample = sampling(1, Z_DIM).cuda()
-    #         x_fake = age_G(z_sample).view(3, 32, 32).cpu()
-    #         im_show(x_fake)
-    #     recon_i = 0
-    #     for i, data in enumerate(trainloader,0):
-    #         x, label = data
-    #         x = x[0].view(1, 3, 32, 32).cuda()
-    #         z = age_E(x)
-    #         x_rec = age_G(z).view(3, 32, 32).cpu()
-    #         im_show(x.view(3, 32, 32).cpu())
-    #         im_show(x_rec)
-    #         recon_i += 1
-    #         if recon_i == 5:
-    #             break
+    plt.plot(enc_z, label='Encoder KL z')
+    plt.plot(enc_fake_z, label='Encoder KL fake z')
+    plt.legend()
+    plt.savefig(f"{plot_dir}/encoder_KL")
+    plt.close()
+
+    plt.plot(gen_fake_z, label='Generator KL fake z')
+    plt.legend()
+    plt.savefig(f"{plot_dir}/generator_KL")
+    plt.close()
+
+    plt.plot(enc_rec_x, label='Encoder x recon')
+    plt.legend()
+    plt.savefig(f"{plot_dir}/encoder_rec")
+    plt.close()
+
+    plt.plot(gen_rec_z, label='Generator z recon')
+    plt.legend()
+    plt.savefig(f"{plot_dir}/generator_rec")
+    plt.close()
 
 
--- AGE/tools.py
+++ AGE/tools.py
@@ -4,6 +4,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+def weights_init(m):
+    '''
+    Custom weights initialization called on netG and netE
+    Code from AGE github
+    '''
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
 def normalize(z):
     # normalize M-dim z to a sphere
     # dim 1 is the batch size
